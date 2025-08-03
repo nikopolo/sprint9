a89af3580b77eb89cfce84d9533d152b998b5f4d
nikopolo <bolliboo@yandex.ru>
2025-08-03 20:55:13 +0200

    first iter

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -9,32 +12,108 @@ const (
 	CHUNKS = 8
 )
 
+var wg sync.WaitGroup
+
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	if size <= 0 {
+		// log.Println("incorrect value")
+		return nil
+	}
+	randomList := make([]int, 0, size)
+	src := rand.NewSource(time.Now().Unix())
+	for i := 0; i < size; i++ {
+		randomList = append(randomList, int(src.Int63()))
+	}
+
+	return randomList
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+
+	max := data[0]
+	for i := 1; i < len(data); i++ {
+		if data[i] > max {
+			max = data[i]
+		}
+	}
+
+	return max
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+
+	size := len(data) / CHUNKS        // размер слайса
+	maxList := make([]int, 0, CHUNKS) // слайс максимумов
+
+	for i := 0; i < CHUNKS; i++ {
+		beginIndex := i * size            // начальный индекс
+		endIndex := beginIndex + size     // конечный индекс
+		list := data[beginIndex:endIndex] // получаемый слайс
+
+		wg.Add(1)
+
+		go func(sl []int) {
+			defer wg.Done()
+
+			max := list[0]
+			for i := 0; i < len(list); i++ {
+				if list[i] > max {
+					max = list[i]
+				}
+			}
+			maxList = append(maxList, max)
+		}(list)
+
+	}
+
+	wg.Wait()
+
+	maxResult := maxList[0]
+	for _, v := range maxList {
+		if v > maxResult {
+			maxResult = v
+		}
+	}
+
+	return maxResult
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	listRand := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
+	begin := time.Now()
+	max := maximum(listRand)
+	elapsed := time.Since(begin).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
+	begin = time.Now()
+	max = maxChunks(listRand)
+	elapsed = time.Since(begin).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,46 @@
 package main
 
+import (
+	"testing"
+)
+
 // Пишите тесты в этом файле
+func TestGenerateRandomElements(t *testing.T) {
+	result := generateRandomElements(0)
+	if result != nil {
+		t.Errorf("expected nil\n")
+	}
+
+	result = generateRandomElements(-1)
+	if result != nil {
+		t.Errorf("expected nil")
+	}
+
+	result = generateRandomElements(1000)
+	if result == nil {
+		t.Errorf("expected slice with %d elements", 1000)
+	}
+}
+
+func TestMaximum(t *testing.T) {
+	//emptyList := []int{}
+	result := maximum([]int{})
+	if result != 0 {
+		t.Error("expected 0")
+	}
+
+	result = maximum([]int{100})
+	if result != 100 {
+		t.Error("expected 100")
+	}
+
+	result = maximum([]int{1, 5, 10})
+	if result != 10 {
+		t.Error("expected 10")
+	}
+
+	result = maximum([]int{5, 5, 5})
+	if result != 5 {
+		t.Error("expected 5")
+	}
+}
